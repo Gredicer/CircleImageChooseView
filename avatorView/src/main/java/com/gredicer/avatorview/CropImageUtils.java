9cd3bdd83ec822f4278e90a6c5ff043669ad55b9
Gredicer <gredicer@outlook.com>
2020-06-14 11:05:31 +0800

    Update class

---
+++ avatorView/src/main/java/com/gredicer/avatorview/CropImageUtils.java
@@ -0,0 +1,214 @@
+package com.gredicer.avatorview;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 裁剪图片工具
+ */
+public class CropImageUtils {
+    //7.0  ContentUri
+    public static String FILE_CONTENT_FILEPROVIDER = null;
+    public static final String APP_NAME = "Gredicer";
+    //打开相机的返回码
+    public static final int REQUEST_CODE_TAKE_PHOTO = 11111;
+    //打开相册的返回码
+    public static final int REQUEST_CODE_SELECT_PICTURE = 11112;
+    //剪切图片的返回码
+    public static final int REQUEST_CODE_CROP_PICTURE = 11113;
+    //相机拍照默认存储路径
+    public static final String PICTURE_DIR = Environment.getExternalStorageDirectory()
+            + File.separator + "pictures" + File.separator + "cropUtils";
+    public String DATE = "";
+    //照片图片名
+    private String photo_image;
+    //截图图片名
+    private String crop_image;
+    private Activity activity;
+
+    public CropImageUtils(Activity activity) {
+        this.activity = activity;
+    }
+
+    public final static String getFileProviderName(Context context){
+        return context.getPackageName()+".fileprovider";
+    }
+    /**
+     * 打开系统相册
+     */
+    public void openAlbum() {
+        DATE = new SimpleDateFormat("yyyy_MMdd_hhmmss").format(new Date());
+        if (isSdCardExist()) {
+            Intent intent;
+            if (Build.VERSION.SDK_INT < 19) {
+                intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+            } else {
+                intent = new Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            }
+            activity.startActivityForResult(intent, REQUEST_CODE_SELECT_PICTURE);
+        } else {
+            Toast.makeText( activity, R.string.sdcard_no_exist, Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    /**
+     * 打开系统相机
+     */
+    public void takePhoto() {
+        DATE = new SimpleDateFormat("yyyy_MMdd_hhmmss").format(new Date());
+        if (isSdCardExist()) {
+            photo_image = createImagePath(APP_NAME + DATE);
+            File file = new File(photo_image);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            //Android7.0以上URI
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                //添加这一句表示对目标应用临时授权该Uri所代表的文件
+                intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                //通过FileProvider创建一个content类型的Uri
+                Uri uri = FileProvider.getUriForFile(activity, FILE_CONTENT_FILEPROVIDER, file);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+            } else {
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+            }
+            try {
+                activity.startActivityForResult(intent, REQUEST_CODE_TAKE_PHOTO);
+            } catch (ActivityNotFoundException anf) {
+                Toast.makeText( activity, R.string.camera_not_prepared, Toast.LENGTH_SHORT ).show();
+            }
+        } else {
+            Toast.makeText( activity, R.string.sdcard_no_exist, Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    /**
+     * 调用系统剪裁功能
+     */
+    public void cropPicture(String path) {
+        File file = new File(path);
+        if (!file.getParentFile().exists()) {
+            file.getParentFile().mkdirs();
+        }
+        Uri imageUri;
+        Uri outputUri;
+        crop_image = createImagePath(APP_NAME + "_crop_" + DATE);
+
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //添加这一句表示对目标应用临时授权该Uri所代表的文件
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            //TODO:访问相册需要被限制，需要通过FileProvider创建一个content类型的Uri
+            imageUri = FileProvider.getUriForFile(activity, FILE_CONTENT_FILEPROVIDER, file);
+            outputUri = Uri.fromFile(new File(crop_image));
+            //TODO:裁剪整个流程，估计授权一次就好outputUri不需要ContentUri,否则失败
+            //outputUri = FileProvider.getUriForFile(activity, "com.bao.cropimage.fileprovider", new File(crop_image));
+        } else {
+            imageUri = Uri.fromFile(file);
+            outputUri = Uri.fromFile(new File(crop_image));
+        }
+        intent.setDataAndType(imageUri, "image/*");
+        intent.putExtra("crop", "true");
+        //设置宽高比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        //设置裁剪图片宽高
+        intent.putExtra("outputX", 200);
+        intent.putExtra("outputY", 200);
+        intent.putExtra("scale", true);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputUri);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        activity.startActivityForResult(intent, REQUEST_CODE_CROP_PICTURE);
+    }
+
+    /**
+     * 拍照/打开相册/剪裁图片的回调
+     */
+    public void onActivityResult(int requestCode, int resultCode
+            , Intent data, OnResultListener listener) {
+        switch (requestCode) {
+            case REQUEST_CODE_TAKE_PHOTO:
+                if (!TextUtils.isEmpty(photo_image)) {
+                    File file = new File(photo_image);
+                    if (file.isFile() && listener != null)
+                        listener.takePhotoFinish(photo_image);
+                    // 最后通知图库更新
+                    activity.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+                }
+                break;
+            case REQUEST_CODE_SELECT_PICTURE:
+                if (data != null) {
+                    Uri uri = data.getData();
+                    if (uri != null) {
+                        String path = ImageUtils.getRealPathFromUri(activity, uri);
+                        File file = new File(path);
+                        if (file.isFile() && listener != null)
+                            listener.selectPictureFinish(path);
+                    }
+                }
+                break;
+            case REQUEST_CODE_CROP_PICTURE:
+                if (!TextUtils.isEmpty(crop_image)) {
+                    File file = new File(crop_image);
+                    if (file.isFile() && listener != null)
+                        listener.cropPictureFinish(crop_image);
+                    // 最后通知图库更新
+                    activity.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(file)));
+                }
+                break;
+        }
+    }
+
+    /**
+     * 创建图片的存储路径
+     */
+    public static String createImagePath(String imageName) {
+        String dir = PICTURE_DIR;
+        File destDir = new File(dir);
+        if (!destDir.exists()) {
+            destDir.mkdirs();
+        }
+        File file = null;
+        if (!TextUtils.isEmpty(imageName)) {
+            file = new File(dir, imageName + ".jpeg");
+        }
+        return file.getAbsolutePath();
+    }
+
+    /**
+     * 检查SD卡是否存在
+     */
+    public boolean isSdCardExist() {
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
+    public interface OnResultListener {
+        //拍照回调
+        void takePhotoFinish(String path);
+
+        //选择图片回调
+        void selectPictureFinish(String path);
+
+        //截图回调
+        void cropPictureFinish(String path);
+    }
+}
---
+++ avatorView/src/main/java/com/gredicer/avatorview/ImageUtils.java
@@ -0,0 +1,181 @@
+package com.gredicer.avatorview;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Base64;
+
+import androidx.loader.content.CursorLoader;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class ImageUtils {
+
+    /**
+     * 根据图片的Uri获取图片的绝对路径。@uri 图片的uri
+     * @return 如果Uri对应的图片存在,那么返回该图片的绝对路径,否则返回null
+     */
+    public static String getRealPathFromUri(Context context, Uri uri) {
+        if(context == null || uri == null) {
+            return null;
+        }
+        if("file".equalsIgnoreCase(uri.getScheme())) {
+            return getRealPathFromUri_Byfile(context,uri);
+        } else if("content".equalsIgnoreCase(uri.getScheme())) {
+            return getRealPathFromUri_Api11To18(context,uri);
+        }
+        return getRealPathFromUri_AboveApi19(context, uri);//没用到
+    }
+
+    //针对图片URI格式为Uri:: file:///storage/emulated/0/DCIM/Camera/IMG_20170613_132837.jpg
+    private static String getRealPathFromUri_Byfile(Context context,Uri uri){
+        String uri2Str = uri.toString();
+        String filePath = uri2Str.substring(uri2Str.indexOf(":") + 3);
+        return filePath;
+    }
+
+    /**
+     * 适配api19以上,根据uri获取图片的绝对路径
+     */
+    @SuppressLint("NewApi")
+    private static String getRealPathFromUri_AboveApi19(Context context, Uri uri) {
+        String filePath = null;
+        String wholeID = null;
+
+        wholeID = DocumentsContract.getDocumentId(uri);
+
+        // 使用':'分割
+        String id = wholeID.split(":")[1];
+
+        String[] projection = { MediaStore.Images.Media.DATA };
+        String selection = MediaStore.Images.Media._ID + "=?";
+        String[] selectionArgs = { id };
+
+        Cursor cursor = context.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
+                selection, selectionArgs, null);
+        int columnIndex = cursor.getColumnIndex(projection[0]);
+
+        if (cursor.moveToFirst()) {
+            filePath = cursor.getString(columnIndex);
+        }
+        cursor.close();
+        return filePath;
+    }
+
+    /**
+     * //适配api11-api18,根据uri获取图片的绝对路径。
+     * 针对图片URI格式为Uri:: content://media/external/images/media/1028
+     */
+    private static String getRealPathFromUri_Api11To18(Context context, Uri uri) {
+        String filePath = null;
+        String[] projection = { MediaStore.Images.Media.DATA };
+
+        CursorLoader loader = new CursorLoader(context, uri, projection, null,
+                null, null);
+        Cursor cursor = loader.loadInBackground();
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+            cursor.close();
+        }
+        return filePath;
+    }
+
+    /**
+     * 适配api11以下(不包括api11),根据uri获取图片的绝对路径
+     */
+    private static String getRealPathFromUri_BelowApi11(Context context, Uri uri) {
+        String filePath = null;
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = context.getContentResolver().query(uri, projection,
+                null, null, null);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            filePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+            cursor.close();
+        }
+        return filePath;
+    }
+
+
+    /**
+     * 将图片转换成Base64编码
+     * @param path 待处理图片
+     * @return
+     */
+
+    public static String getImgStr(String path) {
+        //将图片文件转化为字节数组字符串，并对其进行Base64编码处理
+        //imgFile = "D:\\testPhoto.jpg";
+        if(TextUtils.isEmpty(path)){
+            return null;
+        }
+        InputStream is = null;
+        byte[] data = null;
+        String result = null;
+        try{
+            is = new FileInputStream(path);
+            //创建一个字符流大小的数组。
+            data = new byte[is.available()];
+            //写入数组
+            is.read(data);
+            //用默认的编码格式进行编码
+            result = Base64.encodeToString(data,Base64.DEFAULT);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(null !=is){
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+
+    public static String bitmapToBase64(Bitmap bitmap) {
+
+        String result = null;
+        ByteArrayOutputStream baos = null;
+        try {
+            if (bitmap != null) {
+                baos = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+                baos.flush();
+                baos.close();
+
+                byte[] bitmapBytes = baos.toByteArray();
+                result = Base64.encodeToString(bitmapBytes, Base64.DEFAULT);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (baos != null) {
+                    baos.flush();
+                    baos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+
+}
+
+
